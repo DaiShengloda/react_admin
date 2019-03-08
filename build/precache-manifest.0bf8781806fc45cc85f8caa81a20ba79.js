06c441b4ad9612064538a2403930bb9647dfbe90
QH-20160928EBVB\Administrator <379257993@qq.com>
2019-03-08 15:04:57 +0800

    提交build

---
+++ build/precache-manifest.0bf8781806fc45cc85f8caa81a20ba79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "94acebd2719bd9727d22",
+    "url": "/static/js/main.94acebd2.chunk.js"
+  },
+  {
+    "revision": "0162b94968ceda069525",
+    "url": "/static/js/2.0162b949.chunk.js"
+  },
+  {
+    "revision": "94acebd2719bd9727d22",
+    "url": "/static/css/main.e19ee6a1.chunk.css"
+  },
+  {
+    "revision": "0162b94968ceda069525",
+    "url": "/static/css/2.d42a7cb5.chunk.css"
+  },
+  {
+    "revision": "d799630e93340bbb0f2515bb70be9343",
+    "url": "/index.html"
+  }
+];
